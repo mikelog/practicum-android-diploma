c28bf959b32bf71369b4979e4e884742cc0a15f1
Mikhail Loginov <lmi@t-sk.ru>
2026-09-07 19:29:06 +0300

    YP-EP0-1: Настройка build.gradle и прочих зависимостей (#12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
     id("ru.practicum.android.diploma.plugins.developproperties")
 }
 
@@ -34,6 +37,18 @@ android {
     }
     buildFeatures {
         buildConfig = true
+        compose = true
+        viewBinding = true
+    }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+    packaging {
+        resources.excludes += setOf(
+            "META-INF/LICENSE*",
+            "META-INF/AL2.0",
+            "META-INF/LGPL2.1",
+        )
     }
 }
 
@@ -41,6 +56,10 @@ kotlin {
     compilerOptions {
         jvmTarget = JvmTarget.JVM_17
     }
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
 }
 
 dependencies {
@@ -51,7 +70,57 @@ dependencies {
     implementation(libs.material)
     implementation(libs.constraintlayout)
 
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+    implementation(libs.compose.materialIconsExtended)
+    debugImplementation(libs.compose.uiTooling)
+
+    // Networking
+    implementation(platform(libs.network.okhttpBom))
+    implementation(libs.bundles.network)
+    implementation(libs.network.gson)
+
+    // Database
+    implementation(libs.bundles.database)
+    ksp(libs.database.roomCompiler)
+
+    // DI
+    implementation(libs.di.koinCore)
+    implementation(libs.di.koinAndroid)
+    implementation(libs.di.koinCompose)
+
+    // Images
+    implementation(libs.images.coilCompose)
+
+    // Navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.navigation.compose)
+
+    // Coroutines
+    implementation(libs.bundles.coroutines)
+
+    // Lifecycle
+    implementation(libs.bundles.lifecycle)
+
+    // Fragment
+    implementation(libs.fragment.ktx)
+    implementation(libs.fragment.compose)
+
+    // Unit tests
     testImplementation(libs.junit4)
+    testImplementation(libs.test.coroutines)
+    testImplementation(libs.test.archCore)
+    testImplementation(libs.test.turbine)
+    testImplementation(libs.test.mockk)
+    testImplementation(libs.di.koinTest)
+    testImplementation(libs.di.koinTestJunit4)
+
+    // Instrumented tests
     androidTestImplementation(libs.junit.ext)
     androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.compose.uiTestJunit4)
+    debugImplementation(libs.compose.uiTestManifest)
 }
