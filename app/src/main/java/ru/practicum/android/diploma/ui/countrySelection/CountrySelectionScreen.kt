fd2e2e847591f31d35360aea4af7f83d5729ce7f
friendlyNeighbor <bitkinserejka@gmail.com>
2026-09-09 00:03:06 +0300

    YP-EP0-8: добавил базовую навигацию (#20)
    * Первоначальная настройка, добавил экраны Main, Favorites, Team
    
    * Добавил остальные экраны (всего 8 ?)
    
    * убрал пробел
    
    * сделал detectAll
    
    ---------
    
    Co-authored-by: Mikhail Loginov <lmi@t-sk.ru>

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/countrySelection/CountrySelectionScreen.kt
@@ -0,0 +1,45 @@
+package ru.practicum.android.diploma.ui.countrySelection
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.util.navigation.ScreenRoute
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CountrySelectionScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.сountry_selection)) },
+                expandedHeight = 64.dp
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Зашлушка:
+            Button(
+                onClick = { navController.navigate(ScreenRoute.IndustrySelection.route) }
+            ) {
+                Text(text = "NEXT")
+            }
+        }
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,21 +1,17 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.android.diploma.BuildConfig
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.util.navigation.NavGraph
 
 class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setContent {
+            NavGraph()
+        }
 
-        // Пример использования access token для HeadHunter API
-        networkRequestExample(accessToken = BuildConfig.API_ACCESS_TOKEN)
-    }
-
-    private fun networkRequestExample(accessToken: String) {
-        // ...
     }
 
 }
