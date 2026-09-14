7d8e096967f6081c9e5dc08e29f01fe09a77b5d9
friendlyNeighbor <bitkinserejka@gmail.com>
2026-09-14 19:45:51 +0300

    Добавил зависимости Интерактора и Репозитория поиска вакансий в модуль Koin

--- app/src/main/java/ru/practicum/android/diploma/di/MainSearchModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/MainSearchModule.kt
@@ -2,8 +2,30 @@ package ru.practicum.android.diploma.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.impl.SearchVacancyRepositoryImpl
+import ru.practicum.android.diploma.domain.api.SearchVacancyInteractor
+import ru.practicum.android.diploma.domain.api.SearchVacancyRepository
+import ru.practicum.android.diploma.domain.impl.SearchVacancyInteractorImpl
 import ru.practicum.android.diploma.ui.mainsearch.MainSearchViewModel
 
 val mainSearchModule = module {
-    viewModel { MainSearchViewModel() }
+
+    single<SearchVacancyRepository> {
+        SearchVacancyRepositoryImpl(
+            vacancyApiService = get(),
+            connectivityChecker = get(),
+        )
+    }
+
+    factory<SearchVacancyInteractor> {
+        SearchVacancyInteractorImpl(
+            searchVacancyRepository = get(),
+        )
+    }
+
+    viewModel {
+        MainSearchViewModel(
+            searchVacancyInteractor = get(),
+        )
+    }
 }
