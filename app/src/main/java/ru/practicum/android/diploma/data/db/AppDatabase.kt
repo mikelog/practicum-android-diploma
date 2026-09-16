1b03591e608c6eba799241b3f784792cf662d271
friendlyNeighbor <bitkinserejka@gmail.com>
2026-09-16 23:18:37 +0300

    YP-EP3-С: Dao (#146)
    * Dao
    
    * fix fatal error
    
    * fix fatal error
    
    * fix fatal error
    
    ---------
    
    Co-authored-by: Mikhail Loginov <lmi@t-sk.ru>

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import ru.practicum.android.diploma.data.db.dao.FavoriteVacancyDao
 import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
 
 @Database(
     entities = [FavoriteVacancyEntity::class],
-    version = 1,
+    version = 2,
+    exportSchema = false,
 )
 abstract class AppDatabase : RoomDatabase() {
 
+    abstract fun favoriteVacancyDao(): FavoriteVacancyDao
+
     companion object {
         private const val DATABASE_NAME = "vacancy_db.db"
-
         fun create(context: Context): AppDatabase =
             Room.databaseBuilder(
                 context,
